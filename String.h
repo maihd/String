0ea85529fbb5beba11924b0383a93f713d87b1f5
MaiHD <maihd.dev@gmail.com>
2019-11-19 17:23:33 +0700

    forcing no pointer

--- String.h
+++ String.h
@@ -11,19 +11,21 @@
 extern "C" {
 #endif
 
-// NO POINTER for safety
-typedef char* string;
+// NO POINTER & IMMUTABLE for safety
+typedef const char* string;
 
-STRING_API string String(int size, const string format, ...);
+STRING_API string String(string source);
 STRING_API void   StringFree(string target);
 
-STRING_API string StringFormat(string target, const string format, ...);
-STRING_API string StringFormatArgv(string target, const string format, va_list argv);
+STRING_API string StringFormat(int bufferSize, string format, ...);
+STRING_API string StringFormatArgv(int bufferSize, string format, va_list argv);
 
-STRING_API int    StringSize(const string target);
-STRING_API int    StringLength(const string target);
+//STRING_API string StringFormatBuffer(char[] buffer, string format, ...);
+//STRING_API string StringFormatBufferArgv(char[] buffer, string format, va_list argv);
 
-STRING_API int    StringIsValid(const string target);
+STRING_API int    StringLength(string target);
+
+STRING_API int    StringIsHeap(string target);
 
 #ifdef __cplusplus
 }
@@ -39,81 +41,92 @@ STRING_API int    StringIsValid(const string target);
 
 typedef struct StringBuffer
 {
-    int  size;
+    int  length;
+    //int  memref;
+    long memtag;
     char data[];
 } StringBuffer;
 
-static StringBuffer sEmptyStringBuffer;
-static string       sEmptyString = sEmptyStringBuffer.data;
+static string sEmptyString = "";
+static long   sMemoryTag   = (long)sEmptyString;
 
-int StringIsValid(const string target)
+int StringIsHeap(const string target)
 {
-    return target && target != sEmptyString;
+    if (target && target != sEmptyString)
+    {
+        StringBuffer* buffer = (StringBuffer*)target;
+        return buffer->memtag == sMemoryTag;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-string String(int size, const string format, ...)
+StringBuffer* StringBufferNew(int length)
+{
+    StringBuffer* buffer = (StringBuffer*)malloc(length + 1 + sizeof(StringBuffer));
+    buffer->length = length;
+    buffer->memtag = sMemoryTag;
+    return buffer;
+}
+
+string String(string source)
 {
     if (size <= 0)
     {
         return sEmptyStringBuffer.data;
     }
 
-    StringBuffer* buffer = (StringBuffer*)malloc(size + sizeof(StringBuffer));
-    buffer->size = 0;
+    int length = StringLength(source);
+    StringBuffer* buffer = StringBufferNew(length);
     
-    va_list argv;
-    va_start(argv, format);
-    StringFormatArgv(buffer->data, format, argv);
-    va_end(argv);
+    strncpy(buffer->data, length, source);
     
     return buffer->data;
 }
 
 void StringFree(string target)
 {
-    if (StringIsValid(target))
+    if (StringIsHeap(target))
     {
         StringBuffer* buffer = (StringBuffer*)(target - sizeof(StringBuffer));
-        if (buffer != &sEmptyStringBuffer)
-        {
-            free(buffer);
-        }
+        free(buffer);
     }   
 }
 
-string StringFormat(string target, const string format, ...)
+string StringFormat(int bufferSize, string format, ...)
 {
-    if (StringIsValid(target))
-    {
-        va_list argv;
-        va_start(argv, format);
-        StringFormat(target, format, argv);
-        va_end(argv);
-    }
+    StringBuffer* buffer = StringBufferNew(bufferSize);
+
+    va_list argv;
+    va_start(argv, format);
+    int length = (int)vsprintf(target, format, argv);
+    va_end(argv);
+
+    buffer->length = length;
     
+    return buffer->data;
+}
+
+string StringFormatArgv(string target, string format, va_list argv)
+{
+    StringBuffer* buffer = StringBufferNew(bufferSize);
+    buffer->length = vsprintf(target, format, argv);
     return target;
 }
 
-string StringFormatArgv(string target, const string format, va_list argv)
+int StringLength(string target)
 {
-    vsprintf(target, format, argv);
-    return target;
-}
-
-int StringSize(const string target)
-{
-    if (StringIsValid(target))
+    if (StringIsHeap(target))
     {
         StringBuffer* buffer = (StringBuffer*)(target - sizeof(StringBuffer));
-        return buffer->size;
+        return buffer->length;
     }
-
-    return 0;
-}
-
-int StringLength(const string target)
-{
-    return (int)strlen(target);
+    else
+    {
+        return (int)strlen(target);
+    }
 }
 
 #endif // STRIGN_IMPL
