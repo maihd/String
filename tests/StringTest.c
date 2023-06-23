7d845e802876b657963aa6857cb1190941a3dfec
maihd <maihd.dev@gmail.com>
2023-06-23 17:28:23 +0700

    simple coding with 4coder

--- tests/StringTest.c
+++ tests/StringTest.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 
 #define STRING_IMPL
@@ -6,19 +8,25 @@
 int main()
 {
     //StringLength(NULL); // This should be assertion by the runtime
-
+    
     printf("StringLength(\"\") = %d\n", StringLength(""));
-
+    
     const char* staticGreeting = "Hello world, my name is: MaiHD";
-    printf("staticGreeting.length: %d, staticGreeting.content: %s\n", StringLength(staticGreeting), staticGreeting);
-
+    printf("staticGreeting.length: %d, staticGreeting.content: %s\n", StringLength(staticGreeting), 
+           staticGreeting);
+    
     char buffer[1024];
     const char* stackGreeting = StringFrom(buffer, "Hello world, my name is: MaiHD");
-    printf("stackGreeting.length: %d, stackGreeting.content: %s\n", StringLength(stackGreeting), stackGreeting);
-
+    printf("stackGreeting.length: %d, stackGreeting.content: %s\n", StringLength(stackGreeting), 
+           stackGreeting);
+    
     const char* heapGreeting = StringFormat(64, "Hello world, my name is: %s", "MaiHD");
-    printf("heapGreeting.length: %d, heapGreeting.content: %s\n", StringLength(heapGreeting), heapGreeting);
-
+    printf("heapGreeting.length: %d, heapGreeting.content: %s\n",
+           StringLength(heapGreeting), 
+           heapGreeting);
+    
     StringFree(heapGreeting);
     return 0;
 }
+
+//! End of file, need by GCC
